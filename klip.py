Fixed the rotation bug again.. Added return data to klip_dataset

klip.py
@@ -213,7 +213,7 @@ def align_and_scale(img, new_center, old_center=None, scale_factor=1):
     nanpix = np.where(np.isnan(img))
     img_copy = np.copy(img)
     img_copy[nanpix] = minval * 2.0
-    resampled_img = ndimage.map_coordinates(img_copy, [y, x], cval=np.nan)
+    resampled_img = ndimage.map_coordinates(img_copy, [y, x], cval=minval * 2.0)
     resampled_img[np.where(resampled_img <= minval)] = np.nan
     resampled_img[nanpix] = np.nan
 
@@ -239,9 +239,7 @@ def rotate(img, angle, center, new_center=None, flipx=True):
         angle: angle CCW to rotate by (degrees)
         center: 2 element list [x,y] that defines the center to rotate the image to respect to
         new_center: 2 element list [x,y] that defines the new image center after rotation
-        flipx: default is True, which reverses x axis. NOTE: If this is True, a CCW of the the unflipped image
-               needs to be a CW one, so needs to be multiplie by a negative sign.
-
+        flipx: default is True, which reverses x axis.
     Outputs:
         resampled_img: new 2D image
     """
@@ -251,15 +249,6 @@ def rotate(img, angle, center, new_center=None, flipx=True):
     #create the coordinate system of the image to manipulate for the transform
     dims = img.shape
     x, y = np.meshgrid(np.arange(dims[1], dtype=np.float32), np.arange(dims[0], dtype=np.float32))
-    #flip x if needed
-    if flipx is True:
-        x = x[:, ::-1]
-        xp = (x-center[0])*np.cos(angle_rad) - (y-center[1])*np.sin(angle_rad) + center[0]
-        yp = (x-center[0])*np.sin(angle_rad) + (y-center[1])*np.cos(angle_rad) + center[1]
-    else:
-        #do rotation. CW rotation formula to get a CCW of the image
-        xp = (x-center[0])*np.cos(angle_rad) + (y-center[1])*np.sin(angle_rad) + center[0]
-        yp = -(x-center[0])*np.sin(angle_rad) + (y-center[1])*np.cos(angle_rad) + center[1]
 
     #if necessary, move coordinates to new center
     if new_center is not None:
@@ -267,6 +256,15 @@ def rotate(img, angle, center, new_center=None, flipx=True):
         dy = new_center[1] - center[1]
         x -= dx
         y -= dy
+
+    #flip x if needed to get East left of North
+    if flipx is True:
+        x = x[:, ::-1]
+
+    #do rotation. CW rotation formula to get a CCW of the image
+    xp = (x-center[0])*np.cos(angle_rad) + (y-center[1])*np.sin(angle_rad) + center[0]
+    yp = -(x-center[0])*np.sin(angle_rad) + (y-center[1])*np.cos(angle_rad) + center[1]
+
 
     #resample image based on new coordinates
     #scipy uses y,x convention when meshgrid uses x,y
@@ -278,7 +276,7 @@ def rotate(img, angle, center, new_center=None, flipx=True):
     nanpix = np.where(np.isnan(img))
     img_copy = np.copy(img)
     img_copy[nanpix] = minval * 5.0
-    resampled_img_mask = ndimage.map_coordinates(img_copy, [yp, xp], cval=np.nan)
+    resampled_img_mask = ndimage.map_coordinates(img_copy, [yp, xp], cval=minval * 5.0)
     img_copy[nanpix] = 0
     resampled_img = ndimage.map_coordinates(img_copy, [yp, xp], cval=np.nan)
     resampled_img[np.where(resampled_img_mask < minval)] = np.nan

parallelized.py
@@ -463,7 +463,7 @@ def klip_adi_plus_sdi(imgs, centers, parangs, wvs, annuli=5, subsections=4, move
     outputs = []
     #as each is finishing, queue up the aligned data to be processed with KLIP
     for wv_index, wv_value in realigned_index:
-        print("Wavelength {1} with index {0} has finished align and scale. Queuing for KLIP".format(wv_index, wv_value))
+        print("Wavelength {1:.4} with index {0} has finished align and scale. Queuing for KLIP".format(wv_index, wv_value))
 
         #pick out the science images that need PSF subtraction for this wavelength
         scidata_indicies = np.where(wvs == wv_value)[0]
@@ -491,8 +491,7 @@ def klip_adi_plus_sdi(imgs, centers, parangs, wvs, annuli=5, subsections=4, move
             print("{0:.4}% done ({1}/{2} completed)".format(index*100.0/tot_iter, index, tot_iter))
     # TODO: make the process of waiting for all threads to finish better and print progress in both python2 and python3
 
-
-    #close to pool now and make sure there's no processes still runnign (there shouldn't be or else that would be bad)
+    #close to pool now and make sure there's no processes still running (there shouldn't be or else that would be bad)
     print("Closing threadpool")
     tpool.close()
     tpool.join()
@@ -534,6 +533,8 @@ def klip_dataset(dataset, mode='AS', outputdir=".", fileprefix="", annuli=5, sub
 
     Output
         Saved files in the output directory
+        Returns: rot_imgs: (b, N, wv, y, x) 5D cube of KL cutoff modes (b), number of images (N), wavelengths (wv),
+                            and spatial dimensions. Images are derotated.
     """
     #defaullt numbasis if none
     if numbasis is None:
@@ -597,4 +598,4 @@ def klip_dataset(dataset, mode='AS', outputdir=".", fileprefix="", annuli=5, sub
     for KLcutoff, spectral_cube in zip(numbasis, KLmode_spectral_cubes):
         pyfits.writeto(outputdirpath + '/' + fileprefix + "-KL{0}-speccube.fits".format(KLcutoff), spectral_cube, clobber=True)
 
-    return
+    return rot_imgs