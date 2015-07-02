de109ed8b1115b4f115c8658b108ada9a992074b
Jean-Baptiste Ruffio <jruffio@stanford.edu>
2015-07-01 20:02:59 -0700

    Work in progress on pixel statistic. Some cleaning with some sorting of functions in different files.

---
+++ kpp_pdf.py
@@ -0,0 +1,279 @@
+__author__ = 'JB'
+
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
+from scipy.optimize import minimize
+from scipy.optimize import leastsq
+import astropy.io.fits as pyfits
+from astropy.modeling import models, fitting
+from copy import copy
+import warnings
+import matplotlib.pyplot as plt
+
+
+def model_exp(x,m,alpha):
+    return np.exp(-alpha*x-m)
+
+def LSQ_model_exp(x,y,m,alpha):
+    y_model = model_exp(x,m,alpha)
+    return (y-y_model)**2/y_model
+
+def get_pdf_model(data):
+
+    im_std = np.std(data)
+    bins = np.arange(-10.*im_std,10.*im_std,im_std/10.)
+    im_histo = np.histogram(data, bins=bins)[0]
+
+    N_bins = bins.size-1
+    center_bins = 0.5*(bins[0:N_bins]+bins[1:N_bins+1])
+
+    g_init = models.Gaussian1D(amplitude=np.max(im_histo), mean=0.0, stddev=im_std)
+    fit_g = fitting.LevMarLSQFitter()
+    warnings.simplefilter('ignore')
+    g = fit_g(g_init, center_bins, im_histo)
+    print(g.amplitude,g.mean,g.stddev)
+
+    right_side_noZeros = np.where((center_bins > (g.mean+2*g.stddev))*(im_histo != 0))
+    N_right_bins = len(right_side_noZeros[0])
+    left_side_noZeros = np.where((center_bins < (g.mean-2*g.stddev))*(im_histo != 0))
+    N_left_bins = len(left_side_noZeros[0])
+
+    alpha0 = (np.log(im_histo[right_side_noZeros[0][N_right_bins-1]])-np.log(im_histo[right_side_noZeros[0][0]]))/(center_bins[right_side_noZeros[0][0]]-center_bins[right_side_noZeros[0][N_right_bins-1]])
+    m_alpha0 = -np.log(im_histo[right_side_noZeros[0][0]])-alpha0*center_bins[right_side_noZeros[0][0]]
+    param0_rightExp = (m_alpha0,alpha0)
+    alpha0 = (np.log(im_histo[left_side_noZeros[0][N_left_bins-1]])-np.log(im_histo[left_side_noZeros[0][0]]))/(center_bins[left_side_noZeros[0][0]]-center_bins[left_side_noZeros[0][N_left_bins-1]])
+    m_alpha0 = -np.log(im_histo[left_side_noZeros[0][0]])-alpha0*center_bins[left_side_noZeros[0][0]]
+    param0_leftExp = (m_alpha0,alpha0)
+
+    LSQ_func = lambda para: LSQ_model_exp((bins[0:bins.size-1])[right_side_noZeros], im_histo[right_side_noZeros],para[0],para[1])
+    param_fit_rightExp = leastsq(LSQ_func,param0_rightExp)
+    LSQ_func = lambda para: LSQ_model_exp((bins[0:bins.size-1])[left_side_noZeros], im_histo[left_side_noZeros],para[0],para[1])
+    param_fit_leftExp = leastsq(LSQ_func,param0_leftExp)
+
+
+    new_sampling = np.arange(-20.*im_std,20.*im_std,im_std/100.)
+
+    pdf_model_gaussian = g(new_sampling)
+
+    right_side = np.where((new_sampling >= g.mean))
+    left_side = np.where((new_sampling < g.mean))
+    pdf_model_exp = np.zeros(new_sampling.size)
+    pdf_model_exp[right_side] = model_exp(new_sampling[right_side],*param_fit_rightExp[0])
+    pdf_model_exp[left_side] = model_exp(new_sampling[left_side],*param_fit_leftExp[0])
+
+    weights = np.zeros(new_sampling.size)
+    print((g.mean+2*g.stddev))
+    weights[right_side] = np.tanh((new_sampling[right_side]-(g.mean+2*g.stddev))/(0.1*g.stddev))
+    weights[left_side] = np.tanh(-(new_sampling[left_side]-(g.mean-2*g.stddev))/(0.1*g.stddev))
+    weights = 0.5*(weights+1.0)
+
+
+    pdf_model = weights*pdf_model_exp + (1-weights)*pdf_model_gaussian
+
+    pdf_model /= np.sum(pdf_model)
+
+    if 0:
+        fig = 2
+        plt.figure(fig,figsize=(8,8))
+        plt.plot(new_sampling, weights)
+
+    if 0:
+        fig = 1
+        plt.figure(fig,figsize=(8,8))
+        plt.plot(center_bins,np.array(im_histo,dtype="double")/np.sum(im_histo)/(im_std/10.),'bx-', markersize=5,linewidth=3)
+        plt.plot(center_bins,g(center_bins)/np.sum(g(center_bins))/(im_std/10.),'g.')
+        plt.plot(new_sampling,pdf_model,'r--')
+        plt.xlabel('criterion value', fontsize=20)
+        plt.ylabel('Probability of the value', fontsize=20)
+        plt.xlim((-20.* im_std,20.*im_std))
+        plt.grid(True)
+        ax = plt.gca()
+        ax.tick_params(axis='x', labelsize=20)
+        ax.tick_params(axis='y', labelsize=20)
+        ax.legend(['flat cube histogram','flat cube histogram (Gaussian fit)','planets'], loc = 'upper right', fontsize=12)
+        ax.set_yscale('log')
+        plt.ylim((10**-7,10))
+        plt.show()
+
+    return pdf_model,new_sampling
+
+def get_cdf_model(data):
+    pdf_model,sampling = get_pdf_model(data)
+    return np.cumsum(pdf_model),pdf_model,sampling
+
+
+def get_image_PDF(image,IOWA,N,centroid = None):
+    IWA,OWA = IOWA
+    ny,nx = image.shape
+
+    image_mask = np.ones((ny,nx))
+    image_mask[np.where(np.isnan(image))] = 0
+
+    if centroid is None :
+        x_cen = np.ceil((nx-1)/2) ; y_cen = np.ceil((ny-1)/2)
+    else:
+        x_cen, y_cen = centroid
+
+    # Build the x and y coordinates grids
+    x, y = np.meshgrid(np.arange(nx)-x_cen, np.arange(ny)-y_cen)
+    # Calculate the radial distance of each pixel
+    r_grid = abs(x +y*1j)
+    th_grid = np.arctan2(x,y)
+
+    # Define the radii intervals for each annulus
+    r0 = IWA
+    annuli_radii = []
+    while np.sqrt(N/np.pi+r0**2) < OWA:
+        annuli_radii.append((r0,np.sqrt(N/np.pi+r0**2)))
+        r0 = np.sqrt(N/np.pi+r0**2)
+
+    annuli_radii.append((r0,np.max([ny,nx])))
+    N_annuli = len(annuli_radii)
+
+
+    for it, rminmax in enumerate(annuli_radii):
+        r_min,r_max = rminmax
+
+        where_ring = np.where((r_min< r_grid) * (r_grid < r_max) * image_mask)
+
+        #im = copy(image)
+        #im[where_ring] = 1.0
+        #plt.imshow(im,interpolation="nearest")
+        #plt.show()
+
+        #N_inHisto = np.sum(im_histo)
+        #im_histo = im_histo/float(N_inHisto)
+
+        #print(rminmax)
+        data = image[where_ring]
+        cdf_model, pdf_model, sampling = get_cdf_model(data)
+        if 1:
+            fig = 1
+            plt.figure(fig,figsize=(8,8))
+            plt.plot(sampling,pdf_model,'b-',linewidth=3)
+            plt.plot(sampling,1.-cdf_model,'r-',linewidth=3)
+            plt.xlabel('criterion value', fontsize=20)
+            plt.ylabel('Probability of the value', fontsize=20)
+            plt.grid(True)
+            ax = plt.gca()
+            ax.tick_params(axis='x', labelsize=20)
+            ax.tick_params(axis='y', labelsize=20)
+            ax.legend(['flat cube histogram','flat cube histogram (Gaussian fit)','planets'], loc = 'upper right', fontsize=12)
+            ax.set_yscale('log')
+            plt.ylim((10**-7,10))
+            plt.show()
+
+
+    return
+
+
+
+def pdf_model_gaussAsymExp(x,m,var,var2,m_exp,alpha,beta):
+    #m,var,var2,m_exp,alpha,beta = param
+
+    pdf = np.zeros(x.shape)
+
+    where_x_is_greater_than_m = np.where(x >= m)
+    where_x_is_lower_than_m = np.where(x < m)
+
+    pdf[where_x_is_greater_than_m] = np.exp(-(x[where_x_is_greater_than_m]-m)**2/(2*var)*np.exp(-x[where_x_is_greater_than_m]**2/(2*var2)) \
+                                            - (alpha*(x[where_x_is_greater_than_m]-m)+m_exp)*(1.-np.exp(-x[where_x_is_greater_than_m]**2/(2*var2))) )
+    pdf[where_x_is_lower_than_m] = np.exp(-(x[where_x_is_lower_than_m]-m)**2/(2*var)*np.exp(-x[where_x_is_lower_than_m]**2/(2*var2)) \
+                                          - (-beta*(x[where_x_is_lower_than_m]-m)+m_exp)*(1.-np.exp(-x[where_x_is_lower_than_m]**2/(2*var2))) )
+
+    pdf /= np.sum(pdf)
+
+    return pdf
+
+def MIN_pdf_model_gaussAsymExp(x,y,m,var,var2,m_exp,alpha,beta):
+    pdf_model = pdf_model_gaussAsymExp(x,m,var,var2,m_exp,alpha,beta)
+    return np.sum((y-pdf_model)**2/pdf_model)
+
+def LSQ_pdf_model_gaussAsymExp(x,y,m,var,var2,m_exp,alpha,beta):
+    pdf_model = pdf_model_gaussAsymExp(x,m,var,var2,m_exp,alpha,beta)
+    return (y-pdf_model)**2/pdf_model
+
+
+"""
+        if 1:
+            print(rminmax)
+            im_histo_max = np.max(im_histo)
+
+            g_init = models.Gaussian1D(amplitude=np.max(im_histo), mean=0.0, stddev=im_std)
+            fit_g = fitting.LevMarLSQFitter()
+            warnings.simplefilter('ignore')
+            g = fit_g(g_init, bins[0:bins.size-1], im_histo)
+
+            #m,var,var2,m_exp,alpha,beta = param
+            print(g.amplitude,g.mean,g.stddev)
+
+            #im_histo_tmp = cpy(im_histo)
+            right_side = np.where(bins[0:bins.size-1] > (g.mean+2*g.stddev))
+            left_side = np.where(bins[0:bins.size-1] < (g.mean-2*g.stddev))
+            #im_histo_tmp[)]
+
+            param0_rightExp = (0,0)
+            param_fit_rightExp,pcov = curve_fit(pdf_model_exp, (bins[0:bins.size-1])[right_side], im_histo[right_side], p0=param0_rightExp, sigma=None)
+            param0_leftExp = (0,0)
+            param_fit_leftExp,pcov = curve_fit(pdf_model_exp, (bins[0:bins.size-1])[left_side], im_histo[left_side], p0=param0_leftExp, sigma=None)
+            print(param_fit_rightExp,param_fit_leftExp)
+
+            LSQ_func = lambda para: LSQ_pdf_model_exp((bins[0:bins.size-1])[right_side], im_histo[right_side],para[0],para[1])
+            param_fit_rightExp2 = leastsq(LSQ_func,param0_rightExp)
+            LSQ_func = lambda para: LSQ_pdf_model_exp((bins[0:bins.size-1])[left_side], im_histo[left_side],para[0],para[1])
+            param_fit_leftExp2 = leastsq(LSQ_func,param0_leftExp)
+
+
+            #param0 = (g.mean+0.0,g.stddev+0.0,g.stddev+0.0,0,0,0)
+            #param_fit,pcov = curve_fit(pdf_model_gaussAsymExp, bins[0:bins.size-1], im_histo, p0=param0, sigma=None)
+            #print(param_fit)
+
+            #param0 = [g.mean+0.0,g.stddev+0.0,g.stddev+0.0,0,0,0]
+            #MIN_func = lambda para: MIN_pdf_model_gaussAsymExp(bins[0:bins.size-1],im_histo,para[0],para[1],para[2],para[3],para[4],para[5])
+            #param_fit2 = minimize(MIN_func,param0, method='BFGS').x
+
+            param0 = [g.mean+0.0,g.stddev+0.0,g.stddev+0.0,0,0,0]
+            LSQ_func = lambda para: LSQ_pdf_model_gaussAsymExp(bins[0:bins.size-1],im_histo,para[0],para[1],para[2],para[3],para[4],para[5])
+            param_fit3 = leastsq(LSQ_func,param0)
+            print(param_fit3)
+
+            #LSQ_func = lambda para: para1**
+
+            fig = 1
+            plt.figure(fig,figsize=(8,8))
+            plt.plot(bins[0:bins.size-1],im_histo,'bx-', markersize=5,linewidth=3)
+            plt.plot(bins[0:bins.size-1],g(bins[0:bins.size-1]),'c--',linewidth=1)
+            #plt.plot(bins[0:bins.size-1],pdf_model_gaussAsymExp(bins[0:bins.size-1],*param_fit),'r-',linewidth=2)
+            #plt.plot(bins[0:bins.size-1],pdf_model_gaussAsymExp(bins[0:bins.size-1],*param_fit2),'g-',linewidth=2)
+            plt.plot(bins[0:bins.size-1],pdf_model_gaussAsymExp(bins[0:bins.size-1],*param_fit3[0]),'p-',linewidth=2)
+            plt.plot((bins[0:bins.size-1])[right_side],im_histo[right_side],'r.', markersize=8)
+            plt.plot((bins[0:bins.size-1])[left_side],im_histo[left_side],'g.', markersize=8)
+            plt.plot((bins[0:bins.size-1])[right_side],pdf_model_exp((bins[0:bins.size-1])[right_side],*param_fit_rightExp),'r-',linewidth=2)
+            plt.plot((bins[0:bins.size-1])[left_side],pdf_model_exp((bins[0:bins.size-1])[left_side],*param_fit_leftExp),'g-',linewidth=2)
+            plt.plot((bins[0:bins.size-1])[right_side],pdf_model_exp((bins[0:bins.size-1])[right_side],*param_fit_rightExp2[0]),'r--',linewidth=2)
+            plt.plot((bins[0:bins.size-1])[left_side],pdf_model_exp((bins[0:bins.size-1])[left_side],*param_fit_leftExp2[0]),'g--',linewidth=2)
+
+            plt.xlabel('criterion value', fontsize=20)
+            plt.ylabel('Probability of the value', fontsize=20)
+            plt.xlim((-10.* im_std,10.*im_std))
+            plt.grid(True)
+            ax = plt.gca()
+            #ax.text(10.*im_std, 2.0*im_histo_max/5., str(N_high_SNR_planets),
+            #        verticalalignment='bottom', horizontalalignment='right',
+            #        color='red', fontsize=50)
+            #ax.text(3.*im_std, 2.0*im_histo_max/5., str(N_low_SNR_planets),
+            #        verticalalignment='bottom', horizontalalignment='right',
+            #        color='red', fontsize=50)
+            ax.tick_params(axis='x', labelsize=20)
+            ax.tick_params(axis='y', labelsize=20)
+            ax.legend(['flat cube histogram','flat cube histogram (Gaussian fit)','planets'], loc = 'upper right', fontsize=12)
+            #plt.savefig(outputDir+"histo_"+filename+".png", bbox_inches='tight')
+            #plt.clf()
+            #plt.close(fig)
+            ax.set_yscale('log')
+            plt.ylim((10**-7,1))
+            plt.show()
+
+
+"""
---
+++ kpp_utils.py
@@ -0,0 +1,272 @@
+__author__ = 'JB'
+
+import numpy as np
+import astropy.io.fits as pyfits
+from copy import copy
+from scipy.stats import nanmedian
+import scipy.ndimage as ndimage
+import matplotlib.pyplot as plt
+
+def get_occ(image, centroid = None):
+    '''
+    Get the IWA (inner working angle) of the central disk of nans and return the mask corresponding to the inner disk.
+
+    :param image: A GPI image with a disk full of nans at the center.
+    :param centroid: center of the nan disk
+    :return:
+    '''
+    ny,nx = image.shape
+
+    if centroid is None :
+        x_cen = np.ceil((nx-1)/2) ; y_cen = np.ceil((ny-1)/2)
+    else:
+        x_cen, y_cen = centroid
+
+    IWA = 0
+    while np.isnan(image[x_cen,y_cen+IWA]):
+        IWA += 1
+
+    # Build the x and y coordinates grids
+    x, y = np.meshgrid(np.arange(nx)-x_cen, np.arange(ny)-y_cen)
+    # Calculate the radial distance of each pixel
+    r = abs(x +y*1j)
+
+    mask = np.ones((ny,nx))
+    mask[np.where(np.isnan(image))] = np.nan
+
+    inner_mask = copy(mask)
+    inner_mask[np.where(r > IWA+2.)] = 1
+
+    outer_mask = copy(mask)
+    outer_mask[np.where(np.isnan(inner_mask))] = 1
+    OWA = np.min(r[np.where(np.isnan(outer_mask))])
+
+    return IWA,OWA,inner_mask,outer_mask
+
+def mask_known_objects(cube,prihdr,GOI_list_filename, mask_radius = 7):
+
+    if np.size(cube.shape) == 3:
+        nl,ny,nx = cube.shape
+    elif np.size(cube.shape) == 2:
+        ny,nx = cube.shape
+        cube = cube[None,:]
+        nl = 1
+
+    width = 2*mask_radius+1
+    stamp_x_grid, stamp_y_grid = np.meshgrid(np.arange(0,width,1)-width/2,np.arange(0,width,1)-width/2)
+    stamp_mask = np.ones((width,width))
+    r_stamp = abs((stamp_x_grid) +(stamp_y_grid)*1j)
+    stamp_mask[np.where(r_stamp < mask_radius)] = np.nan
+
+    try:
+        # OBJECT: keyword in the primary header with the name of the star.
+        object_name = prihdr['OBJECT'].strip().replace (" ", "_")
+    except:
+        object_name = "UNKNOWN_OBJECT"
+
+    print(object_name)
+
+    candidates_list = []
+
+    with open(GOI_list_filename, 'r') as GOI_list:
+        for myline in GOI_list:
+            if not myline.startswith("#"):
+                GOI_name, status, k,potential_planet,max_val_criter,x_max_pos,y_max_pos, row_id,col_id = myline.rstrip().split(",")
+                if GOI_name == object_name:
+                    candidates_list.append((int(k),bool(potential_planet),float(max_val_criter),float(x_max_pos),float(y_max_pos), int(row_id),int(col_id)))
+
+
+    row_m = np.floor(width/2.0)
+    row_p = np.ceil(width/2.0)
+    col_m = np.floor(width/2.0)
+    col_p = np.ceil(width/2.0)
+
+    for candidate in candidates_list:
+        k,potential_planet,max_val_criter,x_max_pos,y_max_pos, k,l = candidate
+        cube[:,(k-row_m):(k+row_p), (l-col_m):(l+col_p)] = np.tile(stamp_mask,(nl,1,1)) * cube[:,(k-row_m):(k+row_p), (l-col_m):(l+col_p)]
+
+    return np.squeeze(cube)
+
+
+def extract_PSFs(filename, stamp_width = 10, mute = False):
+    '''
+    Extract the PSFs in a pyklip reduced cube in which fake planets have been injected.
+    The position of the fake planets is stored in the headers when pyklip is used.
+    A cube stamp is extracted for each radius and angle and they are all sorted in the out_stamp_PSFs array.
+
+    /!\ The cube is normalized following cube /= np.nanstd(cube[10,:,:])
+    This is because I don't want the very small numbers in Jason's output as he uses contrast units
+
+    :param filename: Name of the file from which the fake planets cube stamps should be extracted.
+    :param stamp_width: Spatial width of the stamps to be extracted around each fake planets.
+    :param mute: If true print some text in the console.
+    :return out_stamp_PSFs: A (nl,stamp_width,stamp_width,nth,nr) array with nl the number of wavelength of the cube,
+            nth the number of section in the klip reduction and nr the number of annuli. Therefore the cube defined by
+            out_stamp_PSFs[:,:,:,0,2] is a cube stamp of the planet in the first section of the third annulus. In order
+            to know what position it exactly corresponds to please look at the FAKPLPAR keyword in the primary headers.
+    '''
+    hdulist = pyfits.open(filename)
+    cube = hdulist[1].data
+    cube /= np.nanstd(cube[10,:,:])
+    if np.size(cube.shape) == 3:
+        nl,ny,nx = cube.shape
+    elif np.size(cube.shape) == 2:
+        ny,nx = cube.shape
+        cube = cube[None,:]
+        nl = 1
+    #slice = hdulist[1].data[2,:,:]
+    #ny,nx = slice.shape
+    prihdr = hdulist[0].header
+    exthdr = hdulist[1].header
+
+    try:
+        # Retrieve the center of the image from the fits keyword.
+        center = [exthdr['PSFCENTX'], exthdr['PSFCENTY']]
+    except:
+        # If the keywords could not be found.
+        if not mute:
+            print("Couldn't find PSFCENTX and PSFCENTY keywords.")
+        center = [(nx-1)/2,(ny-1)/2]
+
+    try:
+        # Retrieve the position of the fake planets from the fits keyword.
+        fakePlparams_str = prihdr['FAKPLPAR']
+    except:
+        # If the keywords could not be found.
+        if not mute:
+            print("ERROR. Couldn't find FAKPLPAR (Fake planets parameters) keyword. Has to quit extract_PSFs().")
+        return 0
+
+    fakePlparams_splitted_str = fakePlparams_str.split(";")
+    planet_angles_str = fakePlparams_splitted_str[6]
+    planet_radii_str = fakePlparams_splitted_str[7]
+    planet_angles =eval(planet_angles_str.split("=")[1])
+    planet_radii =eval(planet_radii_str.split("=")[1])
+
+    nth = np.size(planet_angles)
+    nr = np.size(planet_radii)
+
+    x, y = np.meshgrid(np.arange(nx), np.arange(ny))
+    #x.shape = (x.shape[0] * x.shape[1])
+    #y.shape = (y.shape[0] * y.shape[1])
+    x -= center[0]
+    y -= center[1]
+    x_planets = np.dot(np.array([planet_radii]).transpose(),np.array([np.cos(np.radians(planet_angles))]))
+    y_planets = np.dot(np.array([planet_radii]).transpose(),np.array([np.sin(np.radians(planet_angles))]))
+    #print(x_planets+center[0])
+    #print(y_planets+center[1])
+
+    out_stamp_PSFs = np.zeros((nl,stamp_width,stamp_width,nth,nr))
+
+    for l_id in range(nl):
+        for r_id,r_it in enumerate(planet_radii):
+            for th_id, th_it in enumerate(planet_angles):
+                x_plnt = np.round(x_planets[r_id,th_id]+center[0])
+                y_plnt = np.round(y_planets[r_id,th_id]+center[1])
+
+                out_stamp_PSFs[l_id,:,:,th_id,r_id] = cube[l_id,
+                                                            (y_plnt-np.floor(stamp_width/2.)):(y_plnt+np.ceil(stamp_width/2.)),
+                                                            (x_plnt-np.floor(stamp_width/2.)):(x_plnt+np.ceil(stamp_width/2.))]
+
+        #print(l_id,r_id,r_it,th_id, th_it,x_plnt,y_plnt)
+        #plt.imshow(out_stamp_PSFs[:,:,l_id,th_id,r_id],interpolation = 'nearest')
+        #plt.show()
+
+    return out_stamp_PSFs
+
+def extract_merge_PSFs(filename, radii, thetas, stamp_width = 10):
+    hdulist = pyfits.open(filename)
+    cube = hdulist[1].data
+    if np.size(cube.shape) == 3:
+        nl,ny,nx = cube.shape
+    elif np.size(cube.shape) == 2:
+        ny,nx = cube.shape
+        cube = cube[None,:]
+        nl = 1
+    nth = np.size(thetas)
+    nr = np.size(radii)
+    #slice = hdulist[1].data[2,:,:]
+    #ny,nx = slice.shape
+    prihdr = hdulist[0].header
+    exthdr = hdulist[1].header
+    try:
+        # Retrieve the center of the image from the fits keyword.
+        center = [exthdr['PSFCENTX'], exthdr['PSFCENTY']]
+    except:
+        # If the keywords could not be found.
+        center = [(nx-1)/2,(ny-1)/2]
+
+    x, y = np.meshgrid(np.arange(nx), np.arange(ny))
+    #x.shape = (x.shape[0] * x.shape[1])
+    #y.shape = (y.shape[0] * y.shape[1])
+    x -= center[0]
+    y -= center[1]
+    x_planets = np.dot(np.array([radii]).transpose(),np.array([np.cos(np.radians(thetas))]))
+    y_planets = np.dot(np.array([radii]).transpose(),np.array([np.sin(np.radians(thetas))]))
+
+    out_stamp_PSFs = np.zeros((ny,nx,nth,nr,nl))
+
+    dn = stamp_width
+    for l_id in range(nl):
+        for r_id,r_it in enumerate(radii):
+            for th_id, th_it in enumerate(thetas):
+                x_plnt = np.ceil(x_planets[r_id,th_id]+center[0])
+                y_plnt = np.ceil(y_planets[r_id,th_id]+center[1])
+
+                stamp = cube[l_id,(y_plnt-dn/2):(y_plnt+dn/2),(x_plnt-dn/2):(x_plnt+dn/2)]
+                stamp_x, stamp_y = np.meshgrid(np.arange(dn, dtype=np.float32), np.arange(dn, dtype=np.float32))
+                stamp_x += (x_planets[r_id,th_id]+center[0]-x_plnt)
+                stamp_y += y_planets[r_id,th_id]+center[1]-y_plnt
+                stamp = ndimage.map_coordinates(stamp, [stamp_y, stamp_x])
+
+                #plt.imshow(stamp,interpolation = 'nearest')
+                #plt.show()
+                #return
+
+                if k == 0 and l == 0:
+                    PSFs_stamps = [stamp]
+                else:
+                    PSFs_stamps = np.concatenate((PSFs_stamps,[stamp]),axis=0)
+
+    return np.mean(PSFs_stamps,axis=0)
+
+def gauss2d(x, y, amplitude = 1.0, xo = 0.0, yo = 0.0, sigma_x = 1.0, sigma_y = 1.0, theta = 0, offset = 0):
+    xo = float(xo)
+    yo = float(yo)
+    a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
+    b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
+    c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
+    g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo)
+                            + c*((y-yo)**2)))
+    return g
+
+def subtract_radialMed(image,w,l,center):
+    ny,nx = image.shape
+
+    n_area_x = np.floor(nx/w)
+    n_area_y = np.floor(ny/w)
+
+    x, y = np.meshgrid(np.arange(nx)-center[0], np.arange(ny)-center[1])
+    r_grid = abs(x +y*1j)
+    th_grid = np.arctan2(x,y)
+
+    for p in np.arange(n_area_x):
+        for q in np.arange(n_area_y):
+            #stamp = image[(q*w):((q+1)*w),(p*w):((p+1)*w)]
+            #image[(q*w):((q+1)*w),(p*w):((p+1)*w)] -= nanmedian(stamp)
+
+            r = r_grid[((q+0.5)*w),((p+0.5)*w)]
+            th = th_grid[((q+0.5)*w),((p+0.5)*w)]
+
+            arc_id = np.where(((r-w/2.0) < r_grid) * (r_grid < (r+w/2.0)) * ((th-l/r) < th_grid) * (th_grid < (th+l/r)))
+            image[(q*w):((q+1)*w),(p*w):((p+1)*w)] -= nanmedian(image[arc_id])
+
+            if 0 and p == 50 and q == 50:
+                image[arc_id] = 100
+                print(image[arc_id].size)
+                plt.figure(2)
+                plt.imshow(image, interpolation="nearest")
+                plt.show()
+
+
+    return image
