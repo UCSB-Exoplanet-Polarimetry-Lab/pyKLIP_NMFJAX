ec7bbe91b408b78992d704e75353a3e90c69d38f
Jason Wang <jason.wang@outlook.com>
2015-08-11 14:57:19 -0700

    Minor tweaks to the setup and README for the new setup procedure

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from setuptools import setup
 
-setup(name='pyklip',
-      version='1.1',
-      description='pyKLIP: PSF Subtraction for Exoplanets and Disks',
-      url='https://bitbucket.org/pyKLIP/pyklip',
-      author='pyKLIP Developers',
-      author_email='jwang@astro.berkeley.edu',
-      license='BSD',
-      packages=['pyklip'],
-      zip_safe=False,
-      classifiers=[
-      # Indicate who your project is intended for
-      'Intended Audience :: Science/Research',
-      'Topic :: Scientific/Engineering :: Astronomy',
+setup(
+    name='pyklip',
+    version='1.1',
+    description='pyKLIP: PSF Subtraction for Exoplanets and Disks',
+    url='https://bitbucket.org/pyKLIP/pyklip',
+    author='pyKLIP Developers',
+    author_email='jwang@astro.berkeley.edu',
+    license='BSD',
+    packages=['pyklip'],
+    zip_safe=False,
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
 
-    # Pick your license as you wish (should match "license" above)
-     'License :: OSI Approved :: BSD License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.4',
-    ],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        ],
     keywords='KLIP PSF Subtraction',
     install_requires=['numpy', 'scipy', 'astropy']
     )
