78d1770995d8a9e9bffb9d48c34f9215aa418dee
Jonathan Aguilar <jonathan.aguilar@jhu.edu>
2016-05-23 12:36:25 -0400

    P1640 quicklook cube checker update
    Added subplots of all cube seeing and airmass values to the quicklook image

--- pyklip/instruments/P1640_support/P1640_cube_checker.py
+++ pyklip/instruments/P1640_support/P1640_cube_checker.py
@@ -16,7 +16,11 @@ import warnings
 from multiprocessing import Pool, Process, Queue
 
 import numpy as np
+
+import matplotlib as mpl
+
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 from matplotlib.colors import LogNorm
 
 from astropy.io import fits
@@ -41,7 +45,7 @@ Pseudocode:
 
 
 # open a fits file and draw the cube
-def draw_cube(cube, cube_name, header):
+def draw_cube(cube, cube_name, header, seeing, airmass, cube_ix):
     """
     Make a figure and draw cube slices on it
     """
@@ -49,9 +53,33 @@ def draw_cube(cube, cube_name, header):
     nchan = cube.shape[0]
     max_val = np.max(cube)
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.yaxis.tick_right()
-    fig.subplots_adjust(left=0.3)
+    fig.suptitle(cube_name, fontsize='x-large')
+    gridsize=(8,8)
+    
+    ax_seeing = plt.subplot2grid(gridsize, (4,5), rowspan=2, colspan=3)
+    ax_seeing.plot(seeing, 'b')
+    ax_seeing.set_xlim(xmin=-0.5, xmax=len(seeing)+0.5)
+    ax_seeing.set_yticks(np.sort(seeing)[[0,-1]])
+    ax_seeing.axvline(cube_ix, c='k', ls='--')
+    ax_seeing.axhline(seeing[cube_ix], c='k', ls='--')
+    ax_seeing.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.2f'))
+    ax_seeing.set_title("seeing")
+    ax_airmass = plt.subplot2grid(gridsize, (6,5), rowspan=2, colspan=3)
+    ax_airmass.plot(airmass, 'r')
+    ax_airmass.set_xlim(xmin=-0.5, xmax=len(airmass)+0.5)
+    ax_airmass.set_yticks(np.sort(airmass)[[0,-1]])
+    ax_airmass.axvline(cube_ix, c='k', ls='--')
+    ax_airmass.axhline(airmass[cube_ix], c='k', ls='--')
+    ax_airmass.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.2f'))
+    ax_airmass.set_title("airmass")
+    ax_airmass.set_xlabel("cube #", size='large')
+
+    #ax = fig.add_subplot(111)
+    ax = plt.subplot2grid(gridsize, (1,0),
+                          rowspan=6,
+                          colspan=5)
+#    ax.yaxis.tick_right()
+#    fig.subplots_adjust(left=0.3)
     datainfo = """Exp time: {exptime:.3f}
 Seeing: {seeing:>9.3f}
 Airmass: {airmass:>8.3f}
@@ -61,32 +89,66 @@ Min val: {minval:>8.1f}""".format(exptime=header["EXP_TIME"],
                               airmass=header["INIT_AM"],
                               maxval=np.nanmax(cube),
                               minval=np.nanmin(cube))
-    fig.text(0.01,0.7, datainfo, size='large', family='monospace',
+    
+    fig.text(0.67, 0.85, datainfo, size='large', family='monospace',
              linespacing=2, 
              verticalalignment='top')
+
     plt.draw()
+    plt.tight_layout(pad=1)
     while True:
-        plt.cla()
+        #plt.sca(ax)
+        #plt.cla()
+        ax.clear()
         chan = chan % nchan
         # you're gonna think you want a common scale for all the slices but you're wrong, leave LogNorm alone
         imax = ax.imshow(cube[chan], norm=LogNorm())#vmax=max_val))
-        plt.title("{name}\nChannel {ch:02d}".format(name=cube_name, ch=chan))
+        #ax.set_title("{name}\nChannel {ch:02d}".format(name=cube_name, ch=chan))
+        ax.set_title("Channel {ch:02d}".format(name=cube_name, ch=chan))
         plt.pause(0.25)
         chan += 1
 
+def plot_airmass_and_seeing(fitsfiles):
+    # plot airmass and seeing
+    seeing = [fits.getval(ff,'SEEING') for ff in fitsfiles]
+    airmass = [fits.getval(ff,'INIT_AM') for ff in fitsfiles]
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    axes[0].plot(seeing,'b-')
+    axes[0].axhline(np.mean(seeing), c='b',label='mean seeing')
+    axes[0].set_title("Seeing")
+    axes[1].plot(airmass, 'r-')
+    axes[1].axhline(np.mean(airmass), c='r', label='mean airmass')
+    axes[1].set_title("Airmass")
+    plt.draw()
+
+def usage():
+    print """Required packages:
+    sys, os
+    warnings, multiprocessing
+    numpy, matplotlib
+    astropy
+Usage:
+    python cube_checker.py /path/to/data/cube.fits
+    OR
+    python cube_checker.py space.fits separated.fits sets.fits of.fits paths.fits to.fits cubes.fits
+    OR
+    python cube_checker.py `ls path/to/cubes/*fits`
+    If running from IPython:
+    files = glob.glob("all/the/fits/*fits")
+    %run cube_checker.py {' '.join(files)}
+
+Prints a list of the chosen file paths
+"""
+
+        
 if __name__ == "__main__":
 
     if sys.argv[1] == 'help':
-        print """Usage:
-        python cube_checker.py /path/to/data/cube.fits
-        OR
-        python cube_checker.py space.fits separated.fits sets.fits of.fits paths.fits to.fits cubes.fits
-        If running from IPython:
-        files = glob.glob("all/the/fits/*fits")
-        %run cube_checker.py {' '.join(files)}
-        """
-    
+        usage()
+        
     fitsfiles = sys.argv[1:]
+    seeing = [fits.getval(ff,'SEEING') for ff in fitsfiles]
+    airmass = [fits.getval(ff,'INIT_AM') for ff in fitsfiles]
     
     good_cubes = dict(zip(fitsfiles, [None for f in fitsfiles]))
 
@@ -95,6 +157,9 @@ if __name__ == "__main__":
         #fig = plt.figure()
         repeat = True
         while repeat:
+#            proc_cube_stats = Process(target=plot_airmass_and_seeing,
+#                                         args=[fitsfiles])
+#            proc_cube_stats.start()
             for i, ff in enumerate(fitsfiles):
                 # check file
                 if not os.path.isfile(ff):
@@ -106,7 +171,7 @@ if __name__ == "__main__":
                 cube_name = os.path.splitext(os.path.basename(ff))[0]
                 header = hdulist[0].header
                 # start drawing subprocess
-                p = Process(target=draw_cube, args=(cube, cube_name, header))
+                p = Process(target=draw_cube, args=(cube, cube_name, header, seeing, airmass, i))
                 p.start()
 
                 # print cube information
@@ -123,6 +188,8 @@ if __name__ == "__main__":
                 # close drawing subprocess
                 p.terminate()
                 p.join()
+#            proc_cube_stats.terminate()
+#            proc_cube_stats.join()
             plt.close('all')
             repeat = raw_input("Finished viewing cubes. Print list and quit? Y/n: ").lower()[0]
             if repeat == 'y':
@@ -142,6 +209,6 @@ if __name__ == "__main__":
     #print good_cubes
     print "Good cubes: "
     for i in sorted([key for key, val in good_cubes.iteritems() if val == True]):
-        print i
+        print os.path.abspath(i)
 
 
