Fixed FPF calculation bug I was using the 2-sided FPF to calculate the 1-sided FPF. Essentially doing 4.86 sigma instead of 5 sigma

pyklip/klip.py
@@ -565,7 +565,7 @@ def meas_contrast(dat, iwa, owa, resolution, center=None):
             specklefluxes.append(speckleflux)
 
         #find 5 sigma flux using student-t statistics
-        fpf_flux = t.ppf(0.99999942697, len(specklefluxes)-1, loc=np.mean(specklefluxes), scale=np.std(specklefluxes, ddof=1))
+        fpf_flux = t.ppf(0.99999971334, len(specklefluxes)-1, loc=np.mean(specklefluxes), scale=np.std(specklefluxes, ddof=1))
         contrast.append(fpf_flux)
 
     return seps, np.array(contrast)