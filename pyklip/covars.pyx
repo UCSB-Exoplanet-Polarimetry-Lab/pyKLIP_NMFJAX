a89585e1af95e0143db4290825de694c6f5b3429
Jason Wang <jason.wang@outlook.com>
2016-11-04 15:28:43 -0700

    Sped up covars by mirroring values

--- pyklip/covars.pyx
+++ pyklip/covars.pyx
@@ -24,11 +24,13 @@ def matern32(np.ndarray[DTYPE_t, ndim=1] x_coords, np.ndarray[DTYPE_t, ndim=1] y
     cdef float dist
 
     for j in range(cov_size):
-        for i in range(cov_size):
+        for i in range(j, cov_size):
 
             dist = (x_coords[i]-x_coords[j])**2 + (y_coords[i] - y_coords[j])**2
             dist = np.sqrt(3*dist)/corr_len
-            cov[j,i] = sigmas[i] * sigmas[j] * (1+ dist) * exp(-dist)
+            cov_val = sigmas[i] * sigmas[j] * (1+ dist) * exp(-dist)
+            cov[j,i] = cov_val
+            cov[i,j] = cov_val
 
     return cov
 
@@ -51,9 +53,11 @@ def sq_exp(np.ndarray[DTYPE_t, ndim=1] x_coords, np.ndarray[DTYPE_t, ndim=1] y_c
     cdef float dist
 
     for j in range(cov_size):
-        for i in range(cov_size):
+        for i in range(j, cov_size):
 
             dist_sq = (x_coords[i]-x_coords[j])**2 + (y_coords[i] - y_coords[j])**2
-            cov[j,i] = sigmas[i] * sigmas[j] * np.exp(-dist_sq/2/(corr_len*corr_len))
+            cov_val = sigmas[i] * sigmas[j] * np.exp(-dist_sq/2/(corr_len*corr_len))
+            cov[j,i] = cov_val
+            cov[i,j] = cov_val
 
     return cov
