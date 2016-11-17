9deacd9cddadba7f7bef19e854f4a95741c0b039
Maxwell A. Millar-Blanchaer <maxmb.astro@gmail.com>
2016-11-16 16:19:03 -0800

    Filling in some psflibrary things

--- pyklip/rdi.py
+++ pyklip/rdi.py
@@ -10,17 +10,18 @@ class PSFLibrary(object):
         aligned_center (array-like): a (x,y) coordinate specifying common center the library is aligned to
         master_filenames (np.ndarray): array of N filenames for each frame in the library. Should match with
                                 pyklip.instruments.Data.filenames for cross-matching
-        master_covar (np.ndarray): N x N array of correlations between each 2 frames
+        master_correlation (np.ndarray): N x N array of correlations between each 2 frames
         master_wvs (np.ndarray): N wavelengths for each frame
         dataset (pyklip.instruments.Instrument.Data)
-        covar (np.ndarray): N_data x M array of correlations between each 2 frames where M are the selected frames
+        correlation (np.ndarray): N_data x M array of correlations between each 2 frames where M are the selected frames
                             and N_data is the number of files in the dataset. Along the N_data dimension, files are
                             ordered in the same way as the dataset object
         isgoodpsf: array of N indicating which M PSFs are good for this dataset
+        nfiles: the number of files in the PSF library
 
     """
 
-    def __init__(self, data, aligned_center, filenames, covar_matrix=None, wvs=None, compute_covar=False):
+    def __init__(self, data, aligned_center, filenames, correlation_matrix=None, wvs=None, compute_correlation=False):
         """
 
         Args:
@@ -29,45 +30,59 @@ class PSFLibrary(object):
             aligned_center (array-like): an (x,y) coordinate specifying the common center all files are registered to
             filenames (np.ndarray): a array of N filenames for each file. These should be in the same format as a
                         pyklip.instruments.Instrument.Data.filenames array so that the two can be cross-matched
-            covar_matrix (np.ndarray): an N x N matrix that expresses the correlation between each two frames in library
+            correlation_matrix (np.ndarray): an N x N matrix that expresses the correlation between each two frames in library
             wvs (np.ndarray): array of N wavelengths that correspond to the wavelengths of the library
-            compute_covar (boolean): if True, compute the covariance matrix. Note that this can potentially take a
+            compute_correlation (boolean): if True, compute the correlation matrix. Note that this can potentially take a
                                     long time, so you really should be doing it once and saving it
         """
         # call init() of super class
         super(PSFLibrary, self).__init__()
 
+        # Do some checking to make sure the data, filenames list and correlation_matrix (if it exists)
+        # all have the same number of files. 
+        nfiles_data = np.shape(data)[0]
+        nfiles_list = np.shape(filenames)[0]
+
+        if nfiles_data != nfiles_list:
+            raise AttributeError("The number of files in the data array and filenames list aren't the same. Something is wrong")
+
+        if correlation_matrix is not None:
+            nfiles_correlation = np.shape(correlation_matrix)[0]
+            if nfiles_correlation != nfiles_data: 
+                raise AttributeError("The number of files in the correlation matrix and in the data array aren't the same. Something is wrong")
+
         # generate master list of files and meta data from inputs
         self.master_library = data
         self.aligned_center = aligned_center
         self.master_filenames = filenames
-        self.master_covar = covar_matrix
+        self.master_correlation = correlation_matrix
         self.master_wvs = wvs
         # fields in the context of a specific dataset
         self.dataset = None
-        self.covar = None
+        self.correlation = None
         self.isgoodpsf = None
+        self.nfiles = nfiles_data
 
-        # check if covariance matrix was passed in
-        if covar_matrix is None and not compute_covar:
-            raise AttributeError("You didn't pass in a covariance matrix, which means it needs to be computed. Are you "
+        # check if correlation matrix was passed in
+        if correlation_matrix is None and not compute_correlation:
+            raise AttributeError("You didn't pass in a correlation matrix, which means it needs to be computed. Are you "
                                  "sure you want to do this? (This may take a while if you have 10,000+ files)")
-        elif compute_covar:
-            self._compute_covar()
+        elif compute_correlation:
+            self._compute_correlation()
 
-    def _compute_covar(self):
+    def _compute_correlation(self):
         """
-        Computes the covariance matrix and saves it in self.covar
+        Computes the correlation matrix and saves it in self.master_correlation
 
         """
         pass
 
-    def save_covar(self, filename, format="numpy"):
+    def save_correlation(self, filename, format="numpy"):
         """
-        Saves self.covar to a file specified by filename
+        Saves self.correlation to a file specified by filename
         Args:
-            filename (str): filepath to store the covariance matrix
-            format (str): type of file to store the covariance matrix as. Supports numpy?/fits?/pickle? (TBD)
+            filename (str): filepath to store the correlation matrix
+            format (str): type of file to store the correlation matrix as. Supports numpy?/fits?/pickle? (TBD)
 
         """
         pass
@@ -87,10 +102,12 @@ class PSFLibrary(object):
 
         # we need to exclude bad files and files already in the dataset itself (since that'd be ADI/SDI/etc)
         in_dataset = np.in1d(self.master_filenames, dataset.filenames)
+        
         # don't compare directly with None
         if badfiles is None:
             badfiles = []
         are_bad = np.in1d(self.master_filenames, badfiles)
+        
         # good ones are the ones that don't fall in either category
         isgood = ~in_dataset & ~badfiles
         good = np.where(isgood)
@@ -106,9 +123,9 @@ class PSFLibrary(object):
             dataset_file_indices_in_lib.append(index)
         dataset_file_indices_in_lib = np.array(dataset_file_indices_in_lib)
 
-        # generate a covariance matrix that's N_dataset x N_goodpsfs
-        # the ordering of the covariance matrix also ensures that N_dataset is ordered the same as dataset
-        self.covar = self.master_covar[dataset_file_indices_in_lib, good]
+        # generate a correlation matrix that's N_dataset x N_goodpsfs
+        # the ordering of the correlation matrix also ensures that N_dataset is ordered the same as dataset
+        self.correlation = self.master_correlation[dataset_file_indices_in_lib, good]
 
         # generate a list indicating which files are good
         self.isgoodpsf = isgood
