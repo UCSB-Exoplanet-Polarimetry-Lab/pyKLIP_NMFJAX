c1a06ef1402303de7b1744997621658031c0a95b
Simon <eumon4@berkeley.edu>
2017-03-23 00:25:54 -0700

    changed test_print to test_python2v3 and moved mock test into test_parallelized.

--- tests/test_parallelized_klip.py
+++ tests/test_parallelized_klip.py
@@ -12,6 +12,10 @@ import pyklip.parallelized as parallelized
 import pyklip.instruments.GPI as GPI
 import pyklip.fakes as fakes
 
+import unittest.mock as mock
+from unittest.mock import patch
+import numpy as np
+
 
 testdir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
 
@@ -152,6 +156,35 @@ def test_adi_gpi_klip_dataset_with_fakes(filelist=None):
         assert (np.abs((peakflux/0.7-fake_contrast)/fake_contrast) < 0.5)
 
     print("{0} seconds to run".format(time()-t1))
+
+
+@patch('pyklip.parallelized.klip_parallelized')
+def test_mock_SDI(mock_klip_parallelized):
+    #create a mocked return value for klip_parallelized that returns a4d array of size (b,N,y,x)
+    mock_klip_parallelized.return_value = np.zeros((4,111,281,281))
+
+    # time it
+    t1 = time()
+
+    # grab the files
+    filelist = glob.glob(testdir + os.path.join("data", "S20131210*distorcorr.fits"))
+    assert(len(filelist) == 3)
+
+    # create the dataset object
+    dataset = GPI.GPIData(filelist)
+
+    # run klip parallelized in SDI mode
+    outputdir = testdir
+    prefix = "mock"
+    parallelized.klip_dataset(dataset, outputdir=outputdir, fileprefix=prefix,
+                          annuli=9, subsections=4, movement=1, numbasis=[1,20,50,100],
+                          calibrate_flux=True, mode="SDI")
+
+    mocked_glob = glob.glob(testdir + 'mock*')
+    assert(len(mocked_glob)==5)
+
+    print("{0} seconds to run".format(time()-t1))
+
 
 
 if __name__ == "__main__":
