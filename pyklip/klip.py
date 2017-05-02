b1c4e809d6529ee6922540568dbcd5029f5825eb
jruffio <jruffio@stanford.edu>
2017-05-02 15:02:45 -0700

    Use a smarter nan masking sheme in align_and_sclae (instead of masking 3 pixels everywhere)

--- pyklip/klip.py
+++ pyklip/klip.py
@@ -269,29 +269,30 @@ def align_and_scale(img, new_center, old_center=None, scale_factor=1,dtype=float
         return np.copy(img)
 
 
-    #Code using position of nans in original image, and padding by nan_pad pixels
-    nan_pad = 3.0 #should be a round number
-    nan_mask = np.copy(img) * 0.0
-
-    xround = np.round(x).astype(np.int)
-    yround = np.round(y).astype(np.int)
-
-    vals = np.array([-nan_pad, nan_pad], dtype=np.int)
-
-    for nan_i in range(0, 2):
-        for nan_j in range(0, 2):
-            xx = (xround + vals[nan_i]).clip(min = 0, max = (dims[1]-1))
-            yy = (yround + vals[nan_j]).clip(min = 0, max = (dims[0]-1))
-
-            nan_mask[np.where(np.isnan(img[np.array(yy), np.array(xx)]))] = 1
-
+    #resample image based on new coordinates
+    #scipy uses y,x convention when meshgrid uses x,y
+    #stupid scipy functions can't work with masked arrays (NANs)
+    #and trying to use interp2d with sparse arrays is way to slow
+    #hack my way out of this by picking a really small value for NANs and try to detect them after the interpolation
+    #then redo the transformation setting NaN to zero to reduce interpolation effects, but using the mask we derived
+    minval = np.min([np.nanmin(img), 0.0])
     nanpix = np.where(np.isnan(img))
     medval = np.median(img[np.where(~np.isnan(img))])
     img_copy = np.copy(img)
+    img_copy[nanpix] = minval * 5.0
+    resampled_img_mask = ndimage.map_coordinates(img_copy, [y, x], cval=minval * 5.0)
     img_copy[nanpix] = medval
+    resampled_img = ndimage.map_coordinates(img_copy, [y,x], cval=np.nan)
+    resampled_img[np.where(resampled_img_mask < minval)] = np.nan
+
+    # # JB debug
+    # nanpix = np.where(np.isnan(img))
+    # medval = np.median(img[np.where(~np.isnan(img))])
+    # img_copy = np.copy(img)
+    # img_copy[nanpix] = medval
 
     resampled_img = ndimage.map_coordinates(img_copy, [y, x], cval = np.nan)
-    resampled_img[np.where(nan_mask == 1)] = np.nan
+    resampled_img[nanpix] = np.nan
 
     #broken attempt at using sparse arrays with interp2d. Warning: takes forever to run
     #good_dat = np.where(~(np.isnan(img)))
