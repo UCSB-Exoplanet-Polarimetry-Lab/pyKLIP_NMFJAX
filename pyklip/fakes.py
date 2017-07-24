6320e94649c6142c5ee0bf004f5e5602e9488db9
jruffio <jruffio@stanford.edu>
2017-07-24 15:27:03 -0700

    Inject planet 1 pixel closer to the mask for default spiral pattern

--- pyklip/fakes.py
+++ pyklip/fakes.py
@@ -386,7 +386,7 @@ def generate_dataset_with_fakes(dataset, fake_position_dict, fake_flux_dict, spe
         # PSF_dist = 20 # Distance between PSFs. Actually length of an arc between 2 consecutive PSFs.
         # delta_pa = 180/np.pi*PSF_dist/radius
         pa_list = np.arange(-180.,180.-0.01,delta_th) + pa_shift
-        radii_list = np.array([dr * annuli_it + dataset.IWA + 3.5 for annuli_it in range(annuli)])
+        radii_list = np.array([dr * annuli_it + dataset.IWA + 2.5 for annuli_it in range(annuli)])
         pa_grid, radii_grid = np.meshgrid(pa_list,radii_list)
         # for row_id in range(pa_grid.shape[0]):
         #     pa_grid[row_id,:] = pa_grid[row_id,:] + 30
