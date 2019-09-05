e2d038a2c4d19bd295bc1a21f16963f5f5558e54
Johan Mazoyer <johan.mazoyer@jpl.nasa.gov>
2019-09-04 17:28:38 -0700

    reverse last commit [skip ci]

--- pyklip/fmlib/diskfm.py
+++ pyklip/fmlib/diskfm.py
@@ -983,7 +983,7 @@ def _save_dict_to_hdf5(dic, filename):
         None
 
     """
-    with h5py.File(filename, "wb") as h5file:
+    with h5py.File(filename, "w") as h5file:
         _recursively_save_dict_contents_to_group(h5file, "/", dic)
 
 
@@ -999,7 +999,7 @@ def _load_dict_from_hdf5(filename):
 
     """
 
-    with h5py.File(filename, "rb") as h5file:
+    with h5py.File(filename, "r") as h5file:
         return _recursively_load_dict_contents_from_group(h5file, "/")
 
 
