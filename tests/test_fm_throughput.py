986e195c2a1e53eb535d6675cad267b20fef2532
Jéa Adams <jeaadams@Jeas-Mac-mini.local>
2020-05-18 11:06:31 -0700

    Created test for field dep. corr. in fmpsf

--- tests/test_fm_throughput.py
+++ tests/test_fm_throughput.py
@@ -2,15 +2,17 @@ import os
 import astropy.io.fits as fits
 import numpy as np
 import scipy
+import time
 import glob
 import scipy.ndimage as ndi
 import matplotlib.pylab as plt
+import scipy.interpolate as sinterp
 import pytest
 import pyklip.klip
 import pyklip.fakes as fakes
 import pyklip.fm as fm
 import pyklip.instruments.GPI as GPI
-import pyklip.fmlib.fmpsf as fmpsf
+import pyklip.fmlib.old_fmpsf as fmpsf
 import pyklip.fitpsf as fitpsf
 
 testdir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
@@ -19,14 +21,15 @@ def test_throughput():
     """
     Tests FM coronagraphic throughput correction
     """
+    t1 = time.time()
 
     filelist = glob.glob(testdir + os.path.join("data", "S20131210*distorcorr.fits"))
-    dataset = GPI.GPIData(filelist, highpass=True)
-
-    ## Do I need this stuff?
+    filelist.sort()
+    skipslices = [i for i in range(37) if i != 7 and i != 33]
+    dataset = GPI.GPIData(filelist, highpass=9, skipslices=skipslices)
 
     numwvs = np.size(np.unique(dataset.wvs))
-    assert(numwvs == 2)
+    print(numwvs)
     # generate PSF
     dataset.generate_psfs(boxrad=25//2)
     dataset.psfs /= (np.mean(dataset.spot_flux.reshape([dataset.spot_flux.shape[0] // numwvs, numwvs]), axis=0)[:, None, None])
@@ -44,31 +47,33 @@ def test_throughput():
     guesssep = 0.4267 / GPI.GPIData.lenslet_scale
     guesspa = 212.15
     guessflux = 5e-5
-    
-    #Specify transmission correction parameters
-    trans = np.ones(50)
-    rad = np.arange(start = 0, stop =50, step = 1)
 
+    #Specify transmission correction parameters
+    trans = np.ones(100)
+    trans[0:30]=1000
+    rad = np.arange(start = 0, stop =100, step = 1)
 
     def transmission_corrected(input_stamp, input_dx, input_dy):
+        """
+        input_dx: should be 2d 
+        input_dy: should be 2d
+        """
         distance_from_center = np.sqrt((input_dx)**2+(input_dy)**2)
         trans_at_dist = np.interp(distance_from_center, rad, trans)
         transmission_stamp = trans_at_dist.reshape(input_stamp.shape)
         output_stamp = transmission_stamp*input_stamp
         return output_stamp
 
-
-    # Initiate fm class
     fm_class = fmpsf.FMPlanetPSF(dataset.input.shape, numbasis, guesssep, guesspa, guessflux, dataset.psfs,
-                                 np.unique(dataset.wvs), dataset.dn_per_contrast, star_spt='A6',
-                                 spectrallib=[inputspec], field_dependent_correction=transmission_corrected)
+                                    np.unique(dataset.wvs), dataset.dn_per_contrast, star_spt='A6',
+                                    spectrallib=[inputspec], field_dependent_correction=transmission_corrected)
 
-     # run KLIP-FM
+        # run KLIP-FM
     prefix = "betpic-131210-j-fmpsf"
     fm.klip_dataset(dataset, fm_class, outputdir=testdir, fileprefix=prefix, numbasis=numbasis,
                     annuli=[[guesssep-15, guesssep+15]], subsections=1, padding=0, movement=2, 
                     time_collapse="weighted-mean")
-    
+
     # read in outputs
     output_prefix = os.path.join(testdir, prefix)
     with fits.open(output_prefix + "-fmpsf-KLmodes-all.fits") as fm_hdu:
@@ -77,11 +82,12 @@ def test_throughput():
         fm_centx = fm_hdu[1].header['PSFCENTX']
         fm_centy = fm_hdu[1].header['PSFCENTY']
 
-
+    print("{0} seconds to run".format(time.time()-t1))
     # Check that flux is 0 at center
-    assert(pytest.approx(fm_frame[int(fm_centx)][int(fm_centx)] == 0, rel=1e-3))
+    assert(abs(fm_frame[int(fm_centx)][int(fm_centx)+20]) > 10)
 
-    # Check that flux is 0 at center
+if __name__ == "__main__":
+    test_throughput()
 
 
                             
