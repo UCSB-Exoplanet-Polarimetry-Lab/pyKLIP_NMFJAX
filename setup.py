35c7256b9760d5433438fe6470bdbd12269362f0
Jason Wang <jason.wang@outlook.com>
2021-03-26 17:40:56 -0700

    forgot to update setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyklip',
-    version='2.2',
+    version='2.3',
     description='pyKLIP: PSF Subtraction for Exoplanets and Disks',
     long_description=long_description,
     long_description_content_type="text/markdown",
