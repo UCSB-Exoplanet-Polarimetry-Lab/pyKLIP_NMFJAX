Fixing bug with contrast curve computation

pyklip/klip.py
@@ -546,7 +546,10 @@ def meas_contrast(dat, iwa, owa, resolution, center=None, low_pass_filter=True):
 
         # find 5 sigma flux using student-t statistics
         # Correction based on Mawet et al. 2014
-        fpf_flux = t.ppf(0.99999971334, num_samples-1, scale=noise_std) * np.sqrt(1 + 1./num_samples) + noise_mean
+        if (num_samples != 0):
+            fpf_flux = t.ppf(0.99999971334, num_samples-1, scale=noise_std) * np.sqrt(1 + 1./num_samples) + noise_mean
+        else:
+            fpf_flux = np.nan
         contrast.append(fpf_flux)
 
     return seps, np.array(contrast)